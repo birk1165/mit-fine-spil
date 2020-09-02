912cd28413b3db723a351f4b695fc5b5ed870a34
birk1165 <70586133+birk1165@users.noreply.github.com>
2020-09-02 16:45:05 +0200

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,70 @@
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    projectile = sprites.createProjectileFromSprite(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . 4 4 4 4 . . . . . . 
+        . . . . 4 4 4 5 5 4 4 4 . . . . 
+        . . . 3 3 3 3 4 4 4 4 4 4 . . . 
+        . . 4 3 3 3 3 2 2 2 1 1 4 4 . . 
+        . . 3 3 3 3 3 2 2 2 1 1 5 4 . . 
+        . 4 3 3 3 3 2 2 2 2 2 5 5 4 4 . 
+        . 4 3 3 3 2 2 2 4 4 4 4 5 4 4 . 
+        . 4 4 3 3 2 2 4 4 4 4 4 4 4 4 . 
+        . 4 2 3 3 2 2 4 4 4 4 4 4 4 4 . 
+        . . 4 2 3 3 2 4 4 4 4 4 2 4 . . 
+        . . 4 2 2 3 2 2 4 4 4 2 4 4 . . 
+        . . . 4 2 2 2 2 2 2 2 2 4 . . . 
+        . . . . 4 4 2 2 2 2 4 4 . . . . 
+        . . . . . . 4 4 4 4 . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `, min_sprite, 0, -70)
+})
+sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Enemy, function (sprite, otherSprite) {
+    otherSprite.destroy(effects.fire, 100)
+    sprite.destroy(effects.trail, 100)
+})
+let astroide: Sprite = null
+let projectile: Sprite = null
+let min_sprite: Sprite = null
+min_sprite = sprites.create(img`
+    . . . . . . . c d . . . . . . . 
+    . . . . . . . c d . . . . . . . 
+    . . . . . . . c d . . . . . . . 
+    . . . . . . . c b . . . . . . . 
+    . . . . . . . f f . . . . . . . 
+    . . . . . . . c 3 . . . . . . . 
+    . . . . . . . f f . . . . . . . 
+    . . . . . . . 8 3 . . . . . . . 
+    . . . . . . 8 8 1 a . . . . . . 
+    . . . . . . 8 3 1 a . . . . . . 
+    . f . . . c c c a a a . . . f . 
+    . f . . 8 8 3 3 3 1 a a . . f . 
+    . f 8 f f f c c a a f f a a f . 
+    . f 8 8 8 a a 3 3 3 3 1 3 a f . 
+    8 8 8 8 8 8 a a 3 3 3 1 3 3 a a 
+    8 8 8 8 8 8 a a 3 3 3 3 1 3 a a 
+    `, SpriteKind.Player)
+controller.moveSprite(min_sprite, 100, 100)
+min_sprite.setFlag(SpriteFlag.StayInScreen, true)
+game.onUpdateInterval(1000, function () {
+    astroide = sprites.create(img`
+        . . . . . . . c c c a c . . . . 
+        . . c c b b b a c a a a c . . . 
+        . c c a b a c b a a a b c c . . 
+        . c a b c f f f b a b b b a . . 
+        . c a c f f f 8 a b b b b b a . 
+        . c a 8 f f 8 c a b b b b b a . 
+        c c c a c c c c a b c f a b c c 
+        c c a a a c c c a c f f c b b a 
+        c c a b 6 a c c a f f c c b b a 
+        c a b c 8 6 c c a a a b b c b c 
+        c a c f f a c c a f a c c c b . 
+        c a 8 f c c b a f f c b c c c . 
+        . c b c c c c b f c a b b a c . 
+        . . a b b b b b b b b b b b c . 
+        . . . c c c c b b b b b c c . . 
+        . . . . . . . . c b b c . . . . 
+        `, SpriteKind.Enemy)
+    astroide.y = 0
+    astroide.x = randint(0, scene.screenWidth())
+    astroide.ay = 100
+})
