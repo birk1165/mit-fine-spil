c7c05b2a908fb7aa6737cb31e11c7efbfd2d82f3
birk1165 <70586133+birk1165@users.noreply.github.com>
2021-04-28 15:49:18 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -278,22 +278,22 @@ let astro_list = [img`
     . . . . . . . . . . . . . . . . 
     . . . . . . . . . . . . . . . . 
     `, img`
-    . . . . . . a a c c a a . . . . 
-    . . . . . a 3 3 3 3 3 3 a . . . 
-    . . . . 3 c 3 3 3 3 3 3 c 3 . . 
-    . . . a 3 c d 3 3 3 3 3 c 3 a . 
-    . . . f 3 3 d 3 3 3 3 3 c 3 f . 
-    . . . f 3 3 d 3 3 3 3 3 3 3 f . 
-    . . . f 3 3 d 3 3 3 3 3 3 3 f . 
-    . . . f 3 c 3 d d 3 3 3 c 3 f . 
-    . . . a 3 c a c c c c a c 3 a . 
-    . . . a 3 a c b b b b c a 3 a . 
-    . . . a 3 a b b b b b b a 3 a . 
-    . . . a a a a a a a a a a a a . 
-    . . . f a d a a a a a a d a f . 
-    . . . f a 3 d a a a a d 3 a f . 
-    . . . f f a a a a a a a a f f . 
-    . . . . f f . . . . . . f f . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . f f f f f f f . . . . . 
+    . . . . f . . . . . f . . . . . 
+    . . . . f 2 2 . 2 2 f . . . . . 
+    . . . . f 2 2 . 2 2 f . . . . . 
+    . f f f f f . . . . f f f f f . 
+    . f f f f f . . . . f f f f f . 
+    . f f . . f 7 2 5 7 f f . f f . 
+    . f f . . f 2 5 5 9 . f . f f . 
+    f f f . . f 8 5 7 8 . f . f f f 
+    f . f . . f . . . . . f . f . f 
+    f . f . . f . . f f . f . f . f 
+    . . . . . f . . f f . f . . . . 
+    . . . . . f . . f f . f . . . . 
+    . . . f f f . . f f . f f f f . 
+    . . f f f f . . f f . f f f f f 
     `, img`
     . . . . . . . . . . . . . . . . 
     . . . . . . 6 6 6 6 6 6 6 6 . . 
